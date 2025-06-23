caea175bcefda659cc0a5219d067e168474184bd
Евгений <immortal.27@yandex.ru>
2025-06-23 21:14:07 +0300

    intervals for new functions, timemeter function

--- app.js
+++ app.js
@@ -6,25 +6,46 @@ function StopApp(){
     // CloseDoor();
 
     AppStopped = true;
-    console.log(AppStopped);
+    console.log('Message: "Game stopped". Value: ' + AppStopped);
     StartApp();
+    ///force reload:
+    // setTimeout (function() {window.open('/', '_self')}, 20000); ///force reload disabled
     return AppStopped;
 }
 
-setTimeout(function(){
-setInterval (function CallAddMeteor( ){AddMeteor(); AddStars();}, 1000)}, 12000);
-// let CallInt = setInterval (function CallAddMeteor( ){AddMeteor(); AddStars();}, 1000);
+        // let CallInt = setInterval (function CallAddMeteor( ){AddMeteor(); AddStars();}, 1000);
+        ///set intervals for function of adding items to background 
+        var IntervalCallFunc_AddStarItem_1;
+        var IntervalCallFunc_AddStarItem_2;
+        var IntervalCallFunc_AddStarItem_3;
+        var IntervalCallFunc_AddStarItem_4;
+        var IntervalCallFunc_AddStarItem_5;
+
 function StartApp(){
-    if(AppStopped === false){
+    if(AppStopped === false){ ///game started
+        GameStart();
+        RunAppTimemeter();
         OpenDoor();
         ShowScore();
         ShowBottomPannelWithButtons();
         // DisplayShipOnStart();
         // CallAddMeteor();
-        setTimeout( function(){AddItemsToFixedPathContainer()}, 12000)
-
+        setTimeout( function(){AddItemsToFixedPathContainer(), IncrPositionOfStone(), ComparePath()}, 12000) /// call functions of game process (GameProcess.js)
+        document.getElementById('StartAppButton').children[1].classList.remove('StartAppButtonImgShining');
+                ///add bonuse:///start live counter:
+                var AddLivesBonuseInterval = setInterval (function CallAddMeteor( ){AddLiveCount();}, 13500);
+        setTimeout(function(){
+            IntervalCallFunc_AddStarItem_1 = setInterval (function CallAddMeteor( ){AddMeteor();}, 700)
+            IntervalCallFunc_AddStarItem_2 = setInterval (function CallAddMeteor( ){AddStars();}, 500)
+            IntervalCallFunc_AddStarItem_3 = setInterval (function CallAddMeteor( ){AddStars_3();}, 500)
+            IntervalCallFunc_AddStarItem_4 = setInterval (function CallAddMeteor( ){AddStars_B();}, 15500)
+            IntervalCallFunc_AddStarItem_5 = setInterval (function CallAddMeteor( ){AddHoles_B();}, 8500)
+            ForceRemoveAnimatedItemsFromBG();
+        }, 13500);
     }    
-    if (AppStopped === true){
+    if (AppStopped === true){ ///game stoped
+        GameOver();
+        StopAppTimemeter();
         clearInt();
         function clearInt(){
             // clearInterval(CallInt);
@@ -33,7 +54,47 @@ function StartApp(){
         DisplayRedLights();
         AppStopped = false;
         HideBottomPannelWithButtons();
-    }return AppStopped;
+                        ///Disable ScanPosition of stones - function: ComparePath()
+                        PositionOfStone = 99999;
+                        ///disable loudness of ship and falling stones
+                        CurrentValueOfSoundTail = ZeroLoudness;
+                        SoundOfDamage.volume = ZeroLoudness;
+                        document.getElementById('ShipEngineSound').volume = ZeroLoudness;
+                        document.body.style.backgroundColor = 'rgba(66, 0, 2, 0.445)';
+                        document.getElementById('ShipItemContainer').style.opacity = 0.1;
+                        ///disable bonuse:
+                        clearInterval(AddLivesBonuseInterval);
+                        ///Clear intervals:
+                        ClearAllIntervals();
+
+    }return AppStopped, IntervalCallFunc_AddStarItem_1,
+                        IntervalCallFunc_AddStarItem_2,
+                        IntervalCallFunc_AddStarItem_3,
+                        IntervalCallFunc_AddStarItem_4,
+                        IntervalCallFunc_AddStarItem_5;
+}
+
+function ClearAllIntervals(){
+    clearInterval(IntervalCallFunc_AddStarItem_1);
+    clearInterval(IntervalCallFunc_AddStarItem_2);
+    clearInterval(IntervalCallFunc_AddStarItem_3);
+    clearInterval(IntervalCallFunc_AddStarItem_4);
+    clearInterval(IntervalCallFunc_AddStarItem_5);
+}
+
+
+///Start Output timemeter values with delay (default 13,5 sec)
+function RunAppTimemeter(){
+    setTimeout(() => { CheckStartClick_Timemeter();}, 13500);
+    // CheckStartClick_Timemeter();
+}
+
+function StopAppTimemeter(){
+    setTimeout(() => {CheckStopClick_Timemeter();}, 13500);
+}
+///force reload:
+function ForceReload(){
+    window.open('/', '_self');
 }
 
 // function CallAddMeteor(){
---
+++ js/timemeter_js/TimeMeterFunc.js
@@ -0,0 +1,192 @@
+﻿//Служебные функции секундомера
+////===========================
+//переменные стартовых собитий
+var GetClickStartTimemeter;
+var GetClickStopTimemeter;
+var PauseStatuseTimemeter;
+
+// нулевые значения времени
+var runingSeconds = 0; //max 59
+var v_Seconds = new Date();
+var runingMinutes = 0; //max 59 
+var v_Minutes = new Date();
+var runingHours = 0; //max 23
+var v_Hours = new Date();
+
+//счетчики вызова F_Seconds, F_Minutes, F_Hours
+var callCounter_S;
+var callCounter_M;
+var callCounter_H;
+
+//временные интервалы для функций
+var setFunctionsIntervals;
+
+// отметки времени
+//var OneMinute = new Date();
+//var fullMinute = OneMinute.setSeconds("18");
+
+//проверка вызова функций секундомера
+let CoreFunc;
+let CoreFuncEvent = true;
+let CoreFuncCall;
+let CoreFunctionsEvents; //для вызова нескольких функций одновременно за одно событие
+let CallTimerCoreFunc;
+
+//вывод значений функции Date
+v_Seconds.getSeconds();
+v_Minutes.getMinutes();
+v_Hours.getHours();
+
+//преобразование значений в текст
+var txt_Hours;
+var txt_Minutes;
+var txt_Seconds;
+
+//интервалы вызова функций 
+var setFunctionsIntervals;
+var setCustomiseFunctionsIntervals;
+
+//проверка событий запуска-остановки, задание начальных значений времмени
+function CheckStartClick_Timemeter() {
+    GetClickStartTimemeter = true;
+    PauseStatuseTimemeter = false;
+    GetClickStopTimemeter = false;
+
+    //setInterval(CheckStartClick_Timemeter, 1000);
+
+    setFunctionsIntervals = setInterval(TimemeterMainF, 1000); // интервал 1 секунда
+    CallTimerOutputFunc = addEventListener("click", TimemeterOutput);
+    do {
+        TimemeterMainF();
+    }
+    while (GetClickStopTimemeter == false);
+
+    return GetClickStartTimemeter, GetClickStopTimemeter, PauseStatuseTimemeter, setFunctionsIntervals;
+}
+function CheckStopClick_Timemeter() {
+    GetClickStartTimemeter = false;
+    GetClickStopTimemeter = true;
+    clearInterval(setFunctionsIntervals);
+    TimemeterMainF();
+    return GetClickStopTimemeter, GetClickStartTimemeter, setFunctionsIntervals;
+}
+
+function PauseTimemeterMethod(){
+    if (PauseStatuseTimemeter == true){
+        Pause_Deactivate();
+    } if (PauseStatuseTimemeter == false){
+        Pause_Activate();
+    }
+}
+function Pause_Activate(){
+    GetClickStartTimemeter = false;
+    // setInterval(TimemeterMainF, 0);
+    PauseStatuseTimemeter = true;
+    // PauseTimemeterMethod();
+    TimemeterMainF();
+    return PauseStatuseTimemeter;
+}
+function Pause_Deactivate(){
+    GetClickStartTimemeter = true;
+    // setInterval(TimemeterMainF, 1000);
+    PauseStatuseTimemeter = false;
+    // PauseTimemeterMethod();
+    TimemeterMainF();
+    return PauseStatuseTimemeter;
+}
+
+//функции возвращающие значения единиц времени: дни/часы/минуты/секунды/м.сек.
+//_______________________________________________________________________
+
+function F_Seconds() {
+    //while (runingMinutes < 10)
+    runingSeconds = 1;
+    do {
+        runingSeconds += 1;
+        v_Seconds.setSeconds(runingSeconds);
+        return v_Seconds, runingSeconds;
+    }
+    while (GetClickStopTimemeter != true)
+
+    //v_Seconds.setSeconds("01");
+}
+function F_Minutes() {
+    //v_Minutes.setMinutes(runingMinutes);    
+    v_Minutes.setMinutes("30");
+    return v_Minutes, runingMinutes;
+}
+function F_Hours() {
+    //v_Hours.setHours(runingHours);
+    v_Hours.setHours("23");
+    return v_Hours, runingHours;
+}
+
+function TimemeterOutput() {
+    //получить значения времени
+    //преобразование значений в текст
+    txt_Hours = v_Hours.getHours().toString();
+    txt_Minutes = v_Minutes.getMinutes().toString();
+    txt_Seconds = v_Seconds.getSeconds().toString();
+    VarToText();
+    document.getElementById("GameTimeMeterLabel").innerHTML = txt_Hours + ":" + txt_Minutes + ":" + txt_Seconds;
+    CallTimerCoreFunc.addEventListener("click", TimemeterMainF);
+}
+
+function TimemeterCore() {
+    //v_Hours.setHours(runingHours);
+    v_Hours.setHours("00"); //test
+    //v_Minutes.setMinutes("00"); //test
+    v_Minutes.setMinutes(runingMinutes);
+    v_Seconds.setSeconds(runingSeconds);
+    runingSeconds += 1;
+    if (runingSeconds > 59) {
+        function Up_minutes() {
+            runingSeconds = 0;
+            runingMinutes += 1;
+            return runingSeconds, runingMinutes;
+        }
+        Up_minutes();
+    }
+    TimemeterOutput();
+    return runingSeconds, v_Hours, v_Minutes, v_Seconds, CoreFunc;
+    //return callCounter_S, runingSeconds, v_Hours, v_Minutes, v_Seconds;
+}
+
+// главная функция секундомера TO DO
+function TimemeterMainF() { 
+
+    if (GetClickStartTimemeter == true) {
+        do {
+            TimemeterCore();
+            TimemeterOutput();
+        } while (GetClickStartTimemeter != false);
+
+    } else if (GetClickStopTimemeter == true) {
+        function setEmptyTimemeter() {
+            clearInterval(setFunctionsIntervals);
+            document.getElementById("GameTimeMeterLabel").innerHTML = "00:00:00";
+        }
+        setEmptyTimemeter();
+    }
+}
+
+//преобразование в строку для значений меньше 10 с выводом 0 в начале значения
+function VarToText() {
+    if (txt_Seconds < 10) {
+        txt_Seconds = "0" + txt_Seconds;
+    }
+    if (txt_Minutes < 10) {
+        txt_Minutes = "0" + txt_Minutes;
+    }
+    if (txt_Hours < 10) {
+        txt_Hours = "0" + txt_Hours;
+    }
+    return txt_Hours, txt_Minutes, txt_Seconds;
+}
+
+//Вызов внешних функций
+function call_BG_RandomiseFunc_ForTimemeter(){
+    do {
+        BG_Randomise_Timemter();
+    } while (GetClickStopTimemeter == false);
+}
